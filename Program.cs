619699af53287f7bcf54134e21f71536d65eb596
Anastasiya-wayPS <71492996+Anastasiya-wayPS@users.noreply.github.com>
2020-10-19 11:11:14 +0300

    Lab_1(2)

--- Program.cs
+++ Program.cs
@@ -1,37 +1,22 @@
 ﻿using System;
-using System.Threading;
 
-namespace Laba_1___2__
+namespace Laba_1_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Задание 2");
-            Console.WriteLine(" ");
+            Console.WriteLine("    Задание 1");
+            Console.WriteLine("             ");
+            Console.WriteLine(" 1. Лабораторная работа 1, Разработка консольного приложения");
+            Console.WriteLine(" 2. Громова Анастасия Евгеньевна");
+            Console.WriteLine(" 3. ИНС-б-о-191, 09.02.03");
+            Console.WriteLine(" 4. 5.04.2002");
+            Console.WriteLine(" 5. г.Ставрополь");
+            Console.WriteLine(" 6. Информатика");
+            Console.WriteLine(" 7. Web-дизайн, конкур");
 
-
-            double c = 0;
-            double q = Math.Pow(8,3);
-            int g = 10, b = 4, h = 2, x = 1, y = 3, w = 3;
-
-
-            c = g * h + b * q - x + (y / w);
-
-            Console.WriteLine("c = g*h+b*q^3-x+y/w");
-            Console.WriteLine(" ");
-            Console.WriteLine("Значение переменной: g = {0}", g);
-            Console.WriteLine("Значение переменной: h = {0}", h);
-            Console.WriteLine("Значение переменной: b = {0}", b);
-            Console.WriteLine("Значение переменной: q = {0}", q);
-            Console.WriteLine("Значение переменной: x = {0}", x);
-            Console.WriteLine("Значение переменной: y = {0}", y);
-            Console.WriteLine("Значение переменной: w = {0}", w);
-
-            Console.WriteLine(" ");
-            Console.WriteLine("Ответ: {0}", c);
             Console.ReadKey();
-
         }
     }
 }
